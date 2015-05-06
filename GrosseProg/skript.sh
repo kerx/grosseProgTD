4df8874061d87349f8f731082fe7cfa3505e43b4
td_work <duecker@ibp.rwth-aachen.de>
2015-05-06 13:24:34 +0200

    new

--- GrosseProg/skript.sh
+++ GrosseProg/skript.sh
@@ -1,5 +1,8 @@
-read -p "Geben Sie bitte den Pfad zu ihren Dateien an. Falls die Dateien sich im selben Ordner befinden, kann die Eingabe leer bleiben." pfad
-read -p "Geben Sie die Endung der zu nutzenden Dateien an. Falls alle Dateien genutzt werden sollen, geben Sie bitte * an." endung
-for file in $pfad*.$endung
-	do java -jar GrosseProg.jar $file > "ausgabe"+ date +"%T"
+read -p "Geben Sie bitte den Pfad zu ihren Dateien an. Falls die Dateien sich im selben Ordner befinden, kann die Eingabe leer bleiben: " a
+read -p "Geben Sie die Endung der zu nutzenden Dateien an. Falls alle Dateien genutzt werden sollen, geben Sie bitte * an: " b
+datum=date+%s
+datei=$a*.$b
+echo $datei
+for file in $datei
+	do java -jar GrosseProg10120534.jar $file > "ausgabe"+$datum
 done
